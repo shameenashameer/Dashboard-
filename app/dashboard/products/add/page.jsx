c6874b1dead086d38c92c38ee43b4e70ac4bb9ed
shameena <shameenashameer21195@gmail.com>
2023-12-14 17:18:32 +0530

    add page

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 // import styles from "../../ui/dashboard/users/users.module.css";
-import styles from '../../ui/dashboard/products/addProducts/addProducts.module.css'
+import styles from "../../../ui/dashboard/products/addProducts/addproducts.module.css";
 const addProduct = () => {
   return (
-    <div className={styles.addProduct} >add Product</div>
-  )
-}
+    <div className={styles.addProduct}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="electronic">electronic</option>
+          <option value="jewelery">jewelery</option>
+          <option value="men's clothing">men's clothing</option>
+          <option value="women's clothing">women's clothing</option>
+        </select>
+        <input type="number" placeholder="price" name="price" />
+        <input type="number" placeholder="stock" name="stock" />
+        <input type="text" placeholder="color" name="color" />
+        <input type="text" placeholder="size" name="size" />
+        <textarea
+          name="desc"
+          id="desc"
+          rows="16"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default addProduct
+export default addProduct;
--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const addUser = () => {
+import styles from "../../../ui/dashboard/users/addUsers/addUsers.module.css";
+const addUsers = () => {
   return (
-    <div>addUser</div>
-  )
-}
+    <div className={styles.addUsers}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="username" name="username" required />
+        <input type="email" placeholder="email" name="email" required />
+        <input type="password" placeholder="password" name="password" required />
+        <input type="phone" placeholder="phone" name="phone"  />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>Is Admin</option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true} selected>Is Active?</option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        
+        <textarea
+          name="address"
+          id="address"
+          rows="16"
+          placeholder="Address"
+         
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default addUser
+export default addUsers;
